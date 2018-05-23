0a68e8dfa606ecd6ca8a5a640cf3af507924bbfb
Lingkai.Xu <xlk910728@gmail.com>
2018-05-23 16:29:13 +1000

    move tinymce to /public

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,12 +12,11 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
 # Rails.application.config.assets.precompile += %w( tinymce/* )
-
-Rails.application.config.assets.precompile = ['*.js', '*.css']
-
-#Rails.application.config.assets.css_compressor = :sass
-#Rails.application.config.assets.js_compressor = :uglifier
+# Rails.application.config.assets.precompile = ['*.js', '*.css']
+# Rails.application.config.assets.css_compressor = :sass
+# Rails.application.config.assets.js_compressor = :uglifier
 
 Rails.application.config.generators do |g|
     g.scaffold_stylesheet false
